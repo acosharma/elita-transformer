96415a946fd0e330a6fae16abea4e1f29aee7167
LahmacunBear <89357390+LahmacunBear@users.noreply.github.com>
2023-09-04 15:07:34 +0100

    Added time parameter for inference

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ class Attention2(tf.keras.layers.Layer):
             shape=[self.H, self.w, self.a], trainable=True, initializer='glorot_uniform',
             name='output_weight')
 
-    def call(self, x):
-        n = tf.linspace(0.0, 1.0, tf.shape(x)[-2])
+    def call(self, x, t=1.0):
+        n = tf.linspace(0.0, t, tf.shape(x)[-2])
         p1 = tf.reduce_sum(tf.sin(
             self.pos['a1']*n + self.pos['b1']
             )*self.pos['c'], axis=-2)
@@ -100,10 +100,10 @@ class Model2(tf.keras.models.Model):
         self.norm = tf.keras.layers.LayerNormalization(axis=-1, scale=False, center=False)
         self.dropout_layer = tf.keras.layers.Dropout(self.dropout)
 
-    def call(self, x, training=True):
+    def call(self, x, t=1.0, training=True):
         for i in range(self.N):
             x = x + self.dropout_layer(
-                self.atts[i](self.norm(x)), training=training)
+                self.atts[i](self.norm(x), t=t), training=training)
             x = x + self.dropout_layer(
                 self.lins[i](self.norm(x)), training=training)
         return x
